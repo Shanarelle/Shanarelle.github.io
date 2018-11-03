b9af244ac4f49a7437e1087be636b90edeef8476
Shanarelle <shanarelle@liquidlability.ninja>
2018-11-03 13:09:52 +0000

    change to thumbnail images with option to view full-size in a modal

---
+++ utils/image_resizer.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+import os, sys
+from PIL import Image
+
+# make it third of original size
+REDUCTION = 3
+
+path = sys.argv[1]
+for infile in os.listdir(path):
+	if infile.endswith(".png") or infile.endswith(".jpg"):
+	    outfile = os.path.join(path, 'thumbnails', infile)
+	    print(outfile)
+	    if infile != outfile:
+	        try:
+	        	im = Image.open(path + '/' + infile)
+	        	size = tuple(pix/REDUCTION for pix in im.size)
+	        	print("size: " + str(size))
+	        	im.thumbnail(size)
+	        	im.save(outfile)
+	        except IOError:
+	            print("cannae create thumbnail for", infile)
